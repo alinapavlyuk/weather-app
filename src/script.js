bb2383e2de3465307e2d711547e19e521ab13dff
alinapavlyuk <alino4kap12@gmail.com>
2022-08-30 20:28:08 +0300

    Did a lot of stuff
    -automatically wrote forecast divs by JS;
    -added unit conversion for forecast ;
    -added min/max temp for today by API;
    -added icons for forecast.

--- src/script.js
+++ src/script.js
@@ -52,60 +52,59 @@ if (minutes < 10) {
     minuteDiv.innerHTML = `${minutes}`;
 }
 
-let toCelsius = document.querySelector("#toCelsius");
-let toFahrenheit = document.querySelector("#toFahrenheit");
-let isCelcius = true;
+function setForecastDates() {
+    let daysArr = [1, 2, 3, 4, 5, 6];
+    daysArr.forEach(function (day) {
+        let nextDay = new Date();
+        nextDay.setDate(nextDay.getDate() + day);
 
-function convertToCelcius(event) {
-    event.preventDefault();
-    if (!isCelcius) {
-        let tempText = document.querySelector("#cur-temperature");
-        let temp = tempText.innerHTML;
-        let tempUnit = document.querySelector("#temp-unit");
-        tempText.innerHTML = Math.round(((temp - 32) * 5) / 9);
-        tempUnit.innerHTML = "℃";
-        isCelcius = true;
-    }
+        let nextDayDiv = document.querySelector(`#day-${day}`);
+        let nextDateDiv = document.querySelector(`#date-${day}`);
+
+        let nextDayOfWeek = days[nextDay.getDay()].toLocaleLowerCase();
+        let nextDate = nextDay.getDate();
+        let nextDayMonths = months[nextDay.getMonth()].toLocaleLowerCase();
+        nextDayDiv.innerHTML = nextDayOfWeek;
+        nextDateDiv.innerHTML = `${nextDate} ${nextDayMonths}`;
+    });
 }
 
-function convertToFahrenheit(event) {
-    event.preventDefault();
-    if (isCelcius) {
-        let tempText = document.querySelector("#cur-temperature");
-        let temp = tempText.innerHTML;
-        let tempUnit = document.querySelector("#temp-unit");
-        tempText.innerHTML = Math.round((temp * 9) / 5 + 32);
-        tempUnit.innerHTML = "℉";
-        isCelcius = false;
-    }
+function displayForecast () {
+    let forecastDiv = document.querySelector("#weather-forecast-nextdays");
+    let forecastHTML = forecastDiv.innerHTML;
+    let daysArr = [1, 2, 3, 4, 5, 6];
+    daysArr.forEach(function (day){
+        forecastHTML = forecastHTML + `
+                <div class="row nextday-forecast">
+                    <div class="col-4">
+                        <div class="nextdays-weather">
+                            <img src="" class="icon-nextdays" id="icon-day-${day}"/>
+                            <br/>
+                            <span class="nextdays-temperature">
+                            <strong>
+                                <span id="temp-day-${day}"></span><span id="temp-unit-day-${day}">℃</span>
+                            </strong>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="col-8">
+                        <div class="date">
+                            <div class="day-of-week" id="day-${day}"></div>
+                            <div class="day" id="date-${day}"></div>
+                        </div>
+                    </div>
+                </div>
+                `;
+    })
+    forecastDiv.innerHTML = forecastHTML;
+    setForecastDates();
 }
 
-toCelsius.addEventListener("click", convertToCelcius);
-toFahrenheit.addEventListener("click", convertToFahrenheit);
-
-for (let i = 1; i <= 6; i++) {
-    let nextDay = new Date();
-    nextDay.setDate(nextDay.getDate() + i);
-
-    let nextDayDiv = document.querySelector(`#day-${i}`);
-    let nextDateDiv = document.querySelector(`#date-${i}`);
-
-    let nextDayOfWeek = days[nextDay.getDay()].toLocaleLowerCase();
-    let nextDate = nextDay.getDate();
-    let nextDayMonths = months[nextDay.getMonth()].toLocaleLowerCase();
-    nextDayDiv.innerHTML = nextDayOfWeek;
-    nextDateDiv.innerHTML = `${nextDate} ${nextDayMonths}`;
-}
+displayForecast();
 
 function setCountry(response) {
     let countryDiv = document.querySelector("#country-name");
     countryDiv.innerHTML = response.data.sys.country;
-}
-
-function setMinMaxTemp(response) {
-    //i have no access to those in openweather api
-    let minTempDiv = document.querySelector("#min-temp");
-    let maxTempDiv = document.querySelector("#max-temp");
 }
 
 function setWindSpeed(response) {
@@ -144,11 +143,11 @@ function setWeather(response) {
     console.log(response);
     setTemp(response);
     setCity(response);
-    setMinMaxTemp(response);
     setCountry(response);
     setWindSpeed(response);
     setHumidity(response);
     setWeatherIcon(response);
+    findCityWeatherForecast(response.data.coord);
 }
 
 function findCityСurWeather(city) {
@@ -157,27 +156,41 @@ function findCityСurWeather(city) {
     axios.get(apiUrl).then(setWeather);
 }
 
-//here I found out that I had no access to daily forecast :(
+function setMinMaxTempToday(response) {
+    let minTempDiv = document.querySelector("#min-temp");
+    let maxTempDiv = document.querySelector("#max-temp");
+    minTempDiv.innerHTML = Math.round(response.data.daily[0].temp.min);
+    maxTempDiv.innerHTML = Math.round(response.data.daily[0].temp.max);
+}
 
-// function setForecast(response) {
-//   console.log(response);
-// }
+function setForecast(response) {
+    setMinMaxTempToday(response);
+    console.log(response);
+    let daysArr = [1, 2, 3, 4, 5, 6];
+    daysArr.forEach(function (day){
+       let tempDivNextDay =  document.querySelector(`#temp-day-${day}`);
+       let iconNextDay = document.querySelector(`#icon-day-${day}`);
+       iconNextDay.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.daily[day].weather[0].icon}@2x.png`);
+       iconNextDay.setAttribute("alt", `${response.data.daily[day].description}`);
+       tempDivNextDay.innerHTML = Math.round(response.data.daily[day].temp.day);
+    });
+}
 
-// function findCityWeatherForecast(city) {
-//   let apiKey = `fb33ec4fa16d64739386bd7d1318cf96`;
-//   let apiUrl = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&units=metric&appid=${apiKey}`;
-//   axios.get(apiUrl).then(setForecast);
-// }
+function findCityWeatherForecast(coord) {
+  let apiKey = `a43564c91a6c605aeb564c9ed02e3858`;
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coord.lat}&lon=${coord.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(setForecast);
+}
 
 findCityСurWeather("Kyiv");
-//findCityWeatherForecast("Kyiv");
+findCityWeatherForecast("Kyiv");
 
 function search(event) {
     event.preventDefault();
     let cityInput = document.querySelector("#input-city");
     if (cityInput.value) {
         findCityСurWeather(cityInput.value);
-        // findCityWeatherForecast(cityInput.value);
+        findCityWeatherForecast(cityInput.value);
     }
     cityInput.value = "";
 }
@@ -199,3 +212,50 @@ function searchLocation() {
 
 let geolocationButton = document.querySelector("#geolocation-button");
 geolocationButton.addEventListener("click", searchLocation);
+
+let toCelsius = document.querySelector("#toCelsius");
+let toFahrenheit = document.querySelector("#toFahrenheit");
+let isCelcius = true;
+
+function convertToCelcius(event) {
+    event.preventDefault();
+    if (!isCelcius) {
+        let daysArr = [1, 2, 3, 4, 5, 6];
+        daysArr.forEach(function (day){
+            let tempDivNextDay = document.querySelector(`#temp-day-${day}`);
+            let tempUnitNextDay = document.querySelector(`#temp-unit-day-${day}`);
+            let tempNextDay = tempDivNextDay.innerHTML
+            tempDivNextDay.innerHTML = Math.round(((tempNextDay - 32) * 5) / 9);
+            tempUnitNextDay.innerHTML = "℃";
+        });
+        let tempText = document.querySelector("#cur-temperature");
+        let tempUnit = document.querySelector("#temp-unit");
+        let temp = tempText.innerHTML;
+        tempText.innerHTML = Math.round(((temp - 32) * 5) / 9);
+        tempUnit.innerHTML = "℃";
+        isCelcius = true;
+    }
+}
+
+function convertToFahrenheit(event) {
+    event.preventDefault();
+    if (isCelcius) {
+        let daysArr = [1, 2, 3, 4, 5, 6];
+        daysArr.forEach(function (day){
+            let tempDivNextDay = document.querySelector(`#temp-day-${day}`);
+            let tempUnitNextDay = document.querySelector(`#temp-unit-day-${day}`);
+            let tempNextDay = tempDivNextDay.innerHTML;
+            tempDivNextDay.innerHTML = Math.round((tempNextDay * 9) / 5 + 32);
+            tempUnitNextDay.innerHTML = "℉";
+        });
+        let tempText = document.querySelector("#cur-temperature");
+        let temp = tempText.innerHTML;
+        let tempUnit = document.querySelector("#temp-unit");
+        tempText.innerHTML = Math.round((temp * 9) / 5 + 32);
+        tempUnit.innerHTML = "℉";
+        isCelcius = false;
+    }
+}
+
+toCelsius.addEventListener("click", convertToCelcius);
+toFahrenheit.addEventListener("click", convertToFahrenheit);
